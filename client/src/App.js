c8ef59f4f3967c6440ae439e3d36916bf7e8e9a0
Kostya <goncharovk911@gmail.com>
2020-10-19 21:54:10 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import 'materialize-css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useRoutes } from './routes';
+
+function App() {
+	const routes = useRoutes(false)
+	return (
+		<Router>
+			<div className="container">
+				{routes}
+			</div>
+		</Router>
+	);
+}
+
+export default App;
